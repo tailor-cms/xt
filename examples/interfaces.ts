3947a0b16c19e5623ce0163857d4e0d7838ce085
underscope <underscope@gmail.com>
2023-08-19 14:36:07 +0200

    Update example element

---
+++ examples/interfaces.ts
@@ -0,0 +1,37 @@
+export interface Element {
+  id: number;
+  uid: string;
+  activityId: number;
+  repositoryId: number;
+  contentId: string;
+  contentSignature: string;
+  type: string;
+  position: number;
+  data: ElementData;
+  meta: { [key: string]: unknown };
+  refs: { [key: string]: unknown };
+  linked: boolean;
+  detached: boolean;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+}
+
+export interface ElementData {}
+
+export type DataInitializer = () => ElementData;
+
+export interface ElementManifest {
+  type: string;
+  version: string;
+  name: string;
+  initState: DataInitializer;
+  Edit?: object;
+  TopToolbar?: object;
+  SideToolbar?: object;
+  Display?: object;
+  ui: {
+    icon: string;
+    forceFullWidth: boolean;
+  };
+}
