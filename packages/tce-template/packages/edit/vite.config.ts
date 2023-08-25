ca8b8db550227e52870d7cb0494f130b6dde96cb
underscope <underscope@gmail.com>
2023-08-25 13:58:42 +0200

    Updated build filename

--- packages/tce-template/packages/edit/vite.config.ts
+++ packages/tce-template/packages/edit/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
       name: "Edit",
-      fileName: "edit",
+      fileName: "index",
     },
   },
   plugins: [vue()],
