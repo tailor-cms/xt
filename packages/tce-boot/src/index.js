1a442685b984d9094a183b17b66459e2dfe4d964
underscope <underscope@gmail.com>
2023-08-29 13:05:48 +0200

    Lint tce-boot

--- packages/tce-boot/src/index.js
+++ packages/tce-boot/src/index.js
@@ -1,43 +1,43 @@
-import { createRequire } from "node:module";
-import path from "node:path";
+import { createRequire } from 'node:module';
+import path from 'node:path';
 
-import boxen from "boxen";
-import concurrently from "concurrently";
+import boxen from 'boxen';
+import concurrently from 'concurrently';
 
 const require = createRequire(import.meta.url);
 
 const runtimes = await Promise.all(
-  ["server", "edit", "display"].map(async (name) => {
+  ['server', 'edit', 'display'].map(async (name) => {
     const pkgRef = `@tailor-cms/tce-${name}-runtime/package.json`;
     const pkgPath = await require.resolve(pkgRef);
     return {
       name,
-      path: path.dirname(pkgPath),
+      path: path.dirname(pkgPath)
     };
   })
 );
 
 console.log(
-  boxen("🚀 Teaching Element Kit", {
-    titleAlignment: "center",
+  boxen('🚀 Teaching Element Kit', {
+    titleAlignment: 'center',
     padding: 1,
     margin: 1,
-    borderColor: "cyan",
+    borderColor: 'cyan'
   })
 );
 
 const { PWD } = process.env;
-const baseDir = PWD.slice(0, PWD.indexOf("/node_modules/"));
+const baseDir = PWD.slice(0, PWD.indexOf('/node_modules/'));
 const tceBootEnv = {
   TCE_DISPLAY_DIR: `${baseDir}/packages/display/dist`,
   TCE_EDIT_DIR: `${baseDir}/packages/edit/dist`,
-  TCE_SERVER_DIR: `${baseDir}/packages/server/dist`,
+  TCE_SERVER_DIR: `${baseDir}/packages/server/dist`
 };
 Object.keys(tceBootEnv).forEach((key) => (process.env[key] = tceBootEnv[key]));
 concurrently(
   runtimes.map((runtime, index) => ({
     name: `${runtime.name}-runtime`,
-    prefixColor: ["magenta", "green", "blue"][index],
-    command: `cd ${runtime.path} && pnpm dev`,
+    prefixColor: ['magenta', 'green', 'blue'][index],
+    command: `cd ${runtime.path} && pnpm dev`
   }))
 );
