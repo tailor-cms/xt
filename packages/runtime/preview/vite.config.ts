c83feeb214ac9128930d996b81d0f0f11dae5aba
underscope <underscope@gmail.com>
2023-08-31 08:26:47 +0200

    Update port for runtime preview

--- packages/runtime/preview/vite.config.ts
+++ packages/runtime/preview/vite.config.ts
@@ -4,5 +4,8 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+  },
   plugins: [vue(), vuetify({ autoImport: true })],
 });
