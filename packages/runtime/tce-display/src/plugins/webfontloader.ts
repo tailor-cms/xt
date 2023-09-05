1e1639620d9aab2145de9025c102da075f5a709e
underscope <underscope@gmail.com>
2023-09-05 07:22:55 +0200

    Address webfontloader issues
    - Dynamic import failing

--- packages/runtime/tce-display/src/plugins/webfontloader.ts
+++ packages/runtime/tce-display/src/plugins/webfontloader.ts
@@ -1,13 +1,10 @@
+import webFontLoader from 'webfontloader';
 /**
  * plugins/webfontloader.js
  *
  * webfontloader documentation: https://github.com/typekit/webfontloader
  */
-export async function loadFonts() {
-  const webFontLoader = await import(
-    /* webpackChunkName: "webfontloader" */ 'webfontloader'
-  );
-
+export function loadFonts() {
   webFontLoader.load({
     google: {
       families: ['Roboto:100,300,400,500,700,900&display=swap'],
