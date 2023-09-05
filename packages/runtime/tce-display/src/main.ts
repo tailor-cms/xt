21905b713c3db8676a645aa3bddc7601b3d1af51
underscope <underscope@gmail.com>
2023-09-05 09:51:00 +0200

    Remove webfontloader

--- packages/runtime/tce-display/src/main.ts
+++ packages/runtime/tce-display/src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import { loadFonts } from './plugins/webfontloader';
 import vuetify from './plugins/vuetify';
 
-await loadFonts();
 createApp(App).use(vuetify).mount('#app');
