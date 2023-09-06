98285df4d2451851e431de50474cfd3fdeac55de
underscope <underscope@gmail.com>
2023-09-06 14:56:53 +0200

    Add package build watchers to tce-boot
    - Build upon change and trigger component reload

--- packages/tce-boot/src/index.js
+++ packages/tce-boot/src/index.js
@@ -5,18 +5,46 @@ import boxen from 'boxen';
 import concurrently from 'concurrently';
 
 const require = createRequire(import.meta.url);
+const TERM_COLORS = ['magenta', 'green', 'blue', 'cyan'];
 
 const runtimes = await Promise.all(
-  ['server', 'edit', 'display', 'preview'].map(async (name) => {
+  ['server', 'edit', 'display', 'preview'].map(async (name, index) => {
     const pkgRef = `@tailor-cms/tce-${name}-runtime/package.json`;
     const pkgPath = await require.resolve(pkgRef);
+    const cmdDir = path.dirname(pkgPath);
     return {
-      name,
-      path: path.dirname(pkgPath)
+      name: `${name}-runtime`,
+      prefixColor: TERM_COLORS[index],
+      path: cmdDir,
+      command: `cd ${cmdDir} && pnpm dev`
     };
   })
 );
 
+const { PWD } = process.env;
+const baseDir = PWD.slice(0, PWD.indexOf('/node_modules/'));
+const keyToName = envKey => envKey.match(/TCE_(.*?)_DIR/)[1].toLowerCase();
+
+const tceBootEnv = {
+  TCE_DISPLAY_DIR: `${baseDir}/packages/display`,
+  TCE_EDIT_DIR: `${baseDir}/packages/edit`,
+  TCE_SERVER_DIR: `${baseDir}/packages/server`
+};
+
+// Set env variables for runtimes
+Object.keys(tceBootEnv).forEach((key) =>
+  (process.env[key] = `${tceBootEnv[key]}/dist`));
+
+// Watchers for rebuilding packages upon change
+const watchers =
+  Object.keys(tceBootEnv).map((key, index) =>
+    ({
+      name: keyToName(key),
+      prefixColor: TERM_COLORS[index],
+      command: `cd ${tceBootEnv[key]} && pnpm dev`
+    }));
+
+// Launch runtimes and watchers
 console.log(
   boxen('🚀 Teaching Element Kit', {
     titleAlignment: 'center',
@@ -25,19 +53,7 @@ console.log(
     borderColor: 'cyan'
   })
 );
-
-const { PWD } = process.env;
-const baseDir = PWD.slice(0, PWD.indexOf('/node_modules/'));
-const tceBootEnv = {
-  TCE_DISPLAY_DIR: `${baseDir}/packages/display/dist`,
-  TCE_EDIT_DIR: `${baseDir}/packages/edit/dist`,
-  TCE_SERVER_DIR: `${baseDir}/packages/server/dist`
-};
-Object.keys(tceBootEnv).forEach((key) => (process.env[key] = tceBootEnv[key]));
-concurrently(
-  runtimes.map((runtime, index) => ({
-    name: `${runtime.name}-runtime`,
-    prefixColor: ['magenta', 'green', 'blue', 'cyan'][index],
-    command: `cd ${runtime.path} && pnpm dev`
-  }))
-);
+concurrently([
+  ...runtimes,
+  ...watchers
+]);
