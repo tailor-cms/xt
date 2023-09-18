3f4fc16dacc663fd7629679b7bee5eca24840dd6
underscope <underscope@gmail.com>
2023-09-18 11:19:10 +0200

    Added tce-display runtime hydration

--- packages/runtime/tce-display/src/App.vue
+++ packages/runtime/tce-display/src/App.vue
@@ -5,7 +5,7 @@
         <v-row>
           <v-col>
             <h2 class="mb-2">Display preview</h2>
-            <Display v-bind="dataSeed" />
+            <Display v-if="element.data" v-bind="element" />
           </v-col>
         </v-row>
       </v-container>
@@ -14,7 +14,33 @@
 </template>
 
 <script setup lang="ts">
-import { dataSeed } from './data';
+import { onMounted, ref } from 'vue';
+import ky from 'ky';
+
+const SERVER_HOST = `localhost:${import.meta.env.VITE_TCE_SERVER_PORT || 8030}`;
+const api = ky.create({ prefixUrl: `http://${SERVER_HOST}` });
+const ws = new WebSocket(`ws://${SERVER_HOST}`);
+
+const element: any = ref({});
+
+onMounted(() => {
+  getElement();
+  ws.addEventListener('message', (event) => {
+    element.value = JSON.parse(event.data);
+  });
+});
+
+const getElement = async () => {
+  try {
+    const response = await api('content-element').json();
+    if (response === null) return;
+    element.value = response;
+  } catch (error) {
+    console.log('Error on element get', error);
+    // Retry
+    setTimeout(() => getElement(), 2000);
+  }
+};
 </script>
 
 <style lang="scss" scoped>
