7f4f646d9d572cfe3183a9e66382c37080798a2e
underscope <underscope@gmail.com>
2023-09-21 09:54:53 +0200

    Replace Promise.each

--- packages/tce-boot/src/index.js
+++ packages/tce-boot/src/index.js
@@ -98,12 +98,12 @@ if (!runtimeLog.initialBootAt) {
   await setTimeout(10 * 1000);
   const editRuntime = commands.find(it => it.name === 'edit-runtime');
   const displayRuntime = commands.find(it => it.name === 'display-runtime');
-  await Promise.each([5000, 6000], (timeout) => {
-    return Promise.all([
+  for (const timeout of [5000, 6000]) {
+    await Promise.all([
       restartCmd(editRuntime, 8010, timeout),
       restartCmd(displayRuntime, 8020, timeout)
     ]);
-  });
+  }
   const previewRuntime = commands.find(it => it.name === 'preview-runtime');
   await restartCmd(previewRuntime, 8080, 2000);
   saveRuntimeInit();
