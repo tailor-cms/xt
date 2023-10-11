54ee7cde4324b18db7a76957eed6f9dd055424e6
underscope <underscope@gmail.com>
2023-10-11 17:52:20 +0200

    Add upload example

--- packages/tce-template/packages/edit/src/components/Edit.vue
+++ packages/tce-template/packages/edit/src/components/Edit.vue
@@ -2,11 +2,44 @@
   <div class="tce-container">
     <div>Times clicked: {{ element.data.count }}</div>
     <button @click="increment">Increment</button>
+    <div class="background-input">
+      <label for="backgroundInput">
+        Set background:
+        <input
+          id="backgroundInput"
+          accept="image/png, image/jpeg"
+          class="background-input"
+          type="file"
+          @change="(e) => upload(e)"
+        />
+      </label>
+      <ul v-if="element.data.key" class="upload-details">
+        <li><b>Storage key:</b>{{ element.data.key }}</li>
+        <li><b>Internal url:</b>{{ element.data.url }}</li>
+        <li>
+          <b>Public url:</b>
+          <a :href="element.data.publicUrl" target="_blank">
+            {{ element.data.publicUrl }}
+          </a>
+        </li>
+      </ul>
+    </div>
+    <img
+      v-if="element.data.publicUrl"
+      :src="element.data.publicUrl"
+      alt="Background image"
+      max-height="100px"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import type { InputFileEvent, StorageApi } from '@tailor-cms/cek-common';
+import { createUploadForm } from '@tailor-cms/cek-common';
 import { Element } from 'tce-manifest';
+import { inject } from 'vue';
+
+const storageService = inject('$storageService') as StorageApi;
 
 const props = defineProps<{ element: Element; isFocused: boolean }>();
 const emit = defineEmits(['save']);
@@ -15,6 +48,20 @@ const increment = () => {
   const { data } = props.element;
   const count = data.count + 1;
   emit('save', { ...data, count });
+};
+
+const upload = (e: InputFileEvent) => {
+  const form = createUploadForm(e);
+  if (!form) return;
+  return storageService.upload(form).then((response) => {
+    emit('save', {
+      ...props.element.data,
+      ...response,
+      assets: {
+        backgroundUrl: response.url,
+      },
+    });
+  });
 };
 </script>
 
@@ -28,10 +75,32 @@ const increment = () => {
   font-size: 1rem;
 }
 
+.background-input {
+  margin: 1rem 0;
+}
+
 button {
   margin-top: 1rem;
-  padding: 0.5rem 1rem;
+  padding: 0.125rem 0.625rem;
+  background-color: #eee;
   border: 1px solid #444;
-  background-color: #fff;
+}
+
+.upload-details {
+  padding: 0;
+  list-style: none;
+
+  > li {
+    margin: 1rem 0;
+    padding: 0.5rem;
+    background-color: #ddd;
+    overflow-x: hidden;
+    text-overflow: ellipsis;
+
+    > b {
+      display: inline-block;
+      padding-right: 0.5rem;
+    }
+  }
 }
 </style>
