034189da9fa2662eb9645c58f49b6c28c5b8caa3
underscope <underscope@gmail.com>
2023-10-11 17:06:51 +0200

    Provide $storageService

--- packages/runtime/tce-edit/src/App.vue
+++ packages/runtime/tce-edit/src/App.vue
@@ -55,6 +55,8 @@
 import { ClickOutside } from 'vuetify/lib/directives';
 import ky from 'ky';
 
+import assetApi from './api/asset';
+
 const SERVER_HOST = `localhost:${import.meta.env.VITE_TCE_SERVER_PORT || 8030}`;
 const api = ky.create({ prefixUrl: `http://${SERVER_HOST}` });
 const ws = new WebSocket(`ws://${SERVER_HOST}`);
@@ -62,6 +64,11 @@ const ws = new WebSocket(`ws://${SERVER_HOST}`);
 export default {
   directives: {
     ClickOutside,
+  },
+  provide() {
+    return {
+      $storageService: assetApi,
+    };
   },
   data: () => ({
     element: {},
