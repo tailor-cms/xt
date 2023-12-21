857a6f15ec07d03496ff927cbfa2b559eec7eeaa
underscope <underscope@gmail.com>
2023-12-21 16:29:00 +0100

    Enable configuring preview port and url

--- packages/runtime/preview/vite.config.ts
+++ packages/runtime/preview/vite.config.ts
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vite';
+import defaults from 'lodash/defaults';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
+const applyDefaultEnv = () => {
+  const env = process.env;
+  const ENV_DEFAULTS = {
+    VITE_PREVIEW_RUNTIME_PORT: '8080',
+    VITE_EDIT_RUNTIME_URL: 'http://localhost:8010',
+    VITE_DISPLAY_RUNTIME_URL: 'http://localhost:8020',
+    VITE_SERVER_RUNTIME_URL: 'http://localhost:8030',
+  };
+  defaults(env, ENV_DEFAULTS);
+};
+
 // https://vitejs.dev/config/
 export default defineConfig((): any => {
+  applyDefaultEnv();
   return {
     root: './src',
     server: {
-      host: '0.0.0.0', // Accept connections from any host (Docker)
-      port: 8080,
+      // Accept connections from any host (Docker)
+      host: '0.0.0.0',
+      port: parseInt(process.env.VITE_PREVIEW_RUNTIME_PORT, 10),
     },
     logLevel: 'warn',
     optimizeDeps: { entries: ['./src/main.js'] },
