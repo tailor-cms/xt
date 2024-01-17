470fb822d4e28d812a8274f4b51d032ec7eccc41
underscope <underscope@gmail.com>
2024-01-17 09:01:34 +0100

    Fix issue with URL resolution

--- packages/runtime/tce-edit/src/api/request.ts
+++ packages/runtime/tce-edit/src/api/request.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import urlJoin from 'url-join'
+
+const apiPrefix = '/tce-server';
 
 const config = {
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: urlJoin(window.location.href, apiPrefix),
   headers: { 'Content-Type': 'application/json' },
 };
 const client = axios.create(config);
