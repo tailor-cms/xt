61870829e1554f4a05844b444f0cdd831d0e5c72
rkusan00 <renato.kusanovic@gmail.com>
2024-11-07 14:17:01 +0100

    Cleanup

--- packages/runtime/tce-display/src/components/NotCompositeAlert.vue
+++ packages/runtime/tce-display/src/components/NotCompositeAlert.vue
@@ -2,7 +2,7 @@
   <VAlert
     class="text-left"
     text="Must be configured as composite element by setting `isComposite` in the manifest."
-    title="Cannot use EmbeddedContainer in non composite elements"
+    title="Cannot use EmbeddedContainer in non-composite elements"
     type="error"
     variant="tonal"
   />
--- packages/runtime/tce-edit-vue3/src/components/NotCompositeAlert.vue
+++ packages/runtime/tce-edit-vue3/src/components/NotCompositeAlert.vue
@@ -1,9 +1,8 @@
 <template>
   <VAlert
-    border="start"
     class="text-left"
     text="Must be configured as composite element by setting `isComposite` in the manifest."
-    title="Cannot use EmbeddedContainer in non composite elements"
+    title="Cannot use EmbeddedContainer in non-composite elements"
     type="error"
     variant="tonal"
   />
