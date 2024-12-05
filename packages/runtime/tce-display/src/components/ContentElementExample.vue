804cbf3557d9d7e7035701b4476438cc84417ec1
rkusan00 <renato.kusanovic@gmail.com>
2024-12-05 08:25:45 +0100

    Tweak example embeds

--- packages/runtime/tce-display/src/components/ContentElementExample.vue
+++ packages/runtime/tce-display/src/components/ContentElementExample.vue
@@ -1,17 +1,21 @@
 <template>
-  <VCard
-    :subtitle="`ID: ${element.id}`"
-    :title="element.data.title || 'Untitled'"
-    color="blue-grey-darken-3"
-    prepend-icon="mdi-cube"
-    variant="tonal"
-  >
-    <template #append>
-      <VChip color="primary">{{ element.type }}</VChip>
-    </template>
-  </VCard>
+  <VSheet class="text-center pa-12" color="grey-lighten-5" border rounded>
+    <VAvatar class="mb-4" color="grey-darken-4" size="x-large">
+      <VIcon icon="mdi-cube" size="x-large" />
+    </VAvatar>
+    <div class="text-h5 mb-2">{{ element.type }} Example</div>
+    <div class="text-grey-darken-2 text-subtitle-1">
+      {{ element.data.title }}
+    </div>
+  </VSheet>
 </template>
 
 <script setup lang="ts">
 defineProps<{ element: any }>();
 </script>
+
+<style scoped>
+.text-subtitle-1 {
+  line-height: 1;
+}
+</style>
