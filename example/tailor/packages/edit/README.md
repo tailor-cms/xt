e8021a4616e8e8d0d715112c734bf2ea7b9faaea
rkusan00 <renato.kusanovic@gmail.com>
2025-02-07 10:17:56 +0100

    Remove old tailor example

--- example/tailor/packages/edit/README.md
+++
@@ -1,1 +0,0 @@
-# Content element `Edit` component
--- example/tailor/packages/edit/src/components/SideToolbar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <p>Edit element side toolbar</p>
-  </div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
