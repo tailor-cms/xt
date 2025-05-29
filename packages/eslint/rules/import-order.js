9d4840c3de777a1ee90021b2f71519414789a20d
rkusan00 <renato.kusanovic@gmail.com>
2025-05-29 08:54:16 +0200

    Cleanup

--- packages/eslint/rules/import-order.js
+++ packages/eslint/rules/import-order.js
@@ -1,9 +1,9 @@
 export default {
+  'sort-imports': ['error', { allowSeparatedGroups: true, ignoreCase: true }],
   'import/order': [
     'error',
     { groups: ['external', 'internal'], 'newlines-between': 'always' },
   ],
-  'sort-imports': ['error', { allowSeparatedGroups: true, ignoreCase: true }],
   'import/no-extraneous-dependencies': [
     'error',
     { optionalDependencies: false },
--- packages/eslint/rules/semistandard.js
+++ packages/eslint/rules/semistandard.js
@@ -1,41 +1,13 @@
-import { config, parser } from 'typescript-eslint';
+import { config } from 'typescript-eslint';
 import importPlugin from 'eslint-plugin-import';
-import nodePlugin from 'eslint-plugin-n';
-import promisePlugin from 'eslint-plugin-promise';
+import neostandard from 'neostandard';
 
 import importOrder from './import-order.js';
 
-export default config({
-  extends: [
-    importPlugin.flatConfigs.recommended,
-    importPlugin.flatConfigs.typescript,
-    nodePlugin.configs['flat/recommended-script'],
-    promisePlugin.configs['flat/recommended'],
-  ],
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-      },
-    },
-  },
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    parser,
-  },
+export default config(neostandard(), {
+  plugins: { import: importPlugin },
   rules: {
     semi: ['error', 'always'],
-    'semi-style': ['error', 'last'],
-    'no-extra-semi': 'error',
-    'space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'always',
-        named: 'never',
-        asyncArrow: 'always',
-      },
-    ],
     ...importOrder,
   },
 });
