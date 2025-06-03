aa5f2785e5c942ef4ff24c89a0853845d40d0cda
rkusan00 <renato.kusanovic@gmail.com>
2025-06-03 11:04:38 +0200

    Reuse interfaces

--- example/packages/manifest/src/interfaces.ts
+++ example/packages/manifest/src/interfaces.ts
@@ -1,3 +1,5 @@
+import common from '@tailor-cms/cek-common';
+
 export interface ElementData {
   count: number;
   key?: string;
@@ -5,41 +7,6 @@ export interface ElementData {
   assets?: { backgroundUrl: string };
 }
 
-export interface Element {
-  id: number;
-  uid: string;
-  activityId: number;
-  repositoryId: number;
-  contentId: string;
-  contentSignature: string;
-  type: string;
-  position: number;
-  data: ElementData;
-  meta: { [key: string]: unknown };
-  refs: { [key: string]: unknown };
-  linked: boolean;
-  detached: boolean;
-  createdAt: string;
-  updatedAt: string;
-  deletedAt: string | null;
-}
-
-export type DataInitializer = () => ElementData;
-
-export interface ElementManifest {
-  type: string;
-  version: string;
-  name: string;
-  initState: DataInitializer;
-  Edit?: object;
-  TopToolbar?: object;
-  SideToolbar?: object;
-  Display?: object;
-  ui: {
-    icon: string;
-    forceFullWidth: boolean;
-  };
-  mocks?: {
-    displayContexts: Array<{ name: string; data: any }>;
-  };
-}
+export type DataInitializer = common.DataInitializer<ElementData>;
+export type Element = common.Element<ElementData>;
+export type ElementManifest = common.ElementManifest<ElementData>;
