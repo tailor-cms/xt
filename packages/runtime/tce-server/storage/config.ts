0f325369ccd22da0f6880caad9072fa0705d5e9c
underscope <underscope@gmail.com>
2025-06-11 15:49:11 +0200

    🐐

--- packages/runtime/tce-server/storage/config.ts
+++ packages/runtime/tce-server/storage/config.ts
@@ -1,6 +1,6 @@
 import * as url from 'node:url';
+import { port, url as serverUrl } from '../config';
 import path from 'node:path';
-import { port } from '../config';
 
 const currentDirectory = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -8,7 +8,7 @@ const config = {
   provider: 'filesystem',
   storagePath: path.join(currentDirectory, 'tmp'),
   protocol: 'storage://',
-  origin: `${url}:${port}`,
+  origin: `${serverUrl}:${port}`,
 };
 
 export default config;
