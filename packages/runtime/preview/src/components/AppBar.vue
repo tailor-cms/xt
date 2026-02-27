9f8dcf82ea48f8372a85ec07e3b44ddadb47ac33
rkusan00 <renato.kusanovic@gmail.com>
2026-02-27 08:39:21 +0100

    Cleanup styles

--- packages/runtime/preview/src/components/AppBar.vue
+++ packages/runtime/preview/src/components/AppBar.vue
@@ -1,20 +1,18 @@
 <template>
-  <VAppBar color="blue-grey-darken-4" elevation="5">
+  <VAppBar color="blue-grey-darken-4" elevation="3">
     <template #prepend>
       <VAppBarNavIcon class="ml-2">
         <img :src="logoUrl" alt="Tailor logo" width="36" />
       </VAppBarNavIcon>
     </template>
-    <VAppBarTitle class="ml-2">
-      Content Element Kit
+    <VAppBarTitle class="ml-2" text="Content Element Kit">
       <VChip
+        :text="version"
         class="ml-3 mb-1 text-body-medium"
         color="#23F48B"
         density="compact"
         variant="tonal"
-      >
-        {{ version }}
-      </VChip>
+      />
     </VAppBarTitle>
     <VBtnToggle
       base-color="white"
@@ -22,25 +20,16 @@
       density="compact"
       variant="tonal"
     >
-      <VBtn @click="showEdit">
-        <span>Authoring</span>
-        <VIcon end>mdi-pencil</VIcon>
-      </VBtn>
-      <VBtn @click="showDisplay">
-        <span>End-user</span>
-        <VIcon end>mdi-fullscreen</VIcon>
-      </VBtn>
-      <VBtn @click="showBothComponents">
-        <span>Both</span>
-        <VIcon end>mdi-math-norm-box</VIcon>
-      </VBtn>
+      <VBtn append-icon="mdi-pencil" text="Authoring" @click="showEdit" />
+      <VBtn append-icon="mdi-fullscreen" text="End-user" @click="showDisplay" />
+      <VBtn
+        append-icon="mdi-math-norm-box"
+        text="Both"
+        @click="showBothComponents"
+      />
     </VBtnToggle>
-    <VBtn href="https://github.com/tailor-cms/tce-template" icon>
-      <VIcon>mdi-github</VIcon>
-    </VBtn>
-    <VBtn href="https://tailor-cms.github.io/xt/" icon>
-      <VIcon>mdi-book-outline</VIcon>
-    </VBtn>
+    <VBtn href="https://github.com/tailor-cms/tce-template" icon="mdi-github" />
+    <VBtn href="https://tailor-cms.github.io/xt/" icon="mdi-book-outline" />
   </VAppBar>
 </template>
 
