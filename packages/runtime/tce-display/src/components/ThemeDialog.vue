2879b07ca5544402869a36c8ac64b84f1da42606
rkusan00 <renato.kusanovic@gmail.com>
2026-07-24 21:24:02 +0200

    Fix theme dialogs

--- packages/runtime/tce-display/src/components/ThemeDialog.vue
+++ packages/runtime/tce-display/src/components/ThemeDialog.vue
@@ -9,70 +9,79 @@
         variant="flat"
       />
     </template>
-    <VCard>
-      <VToolbar class="mb-4" title="Theme selector">
-        <VBtn aria-label="Close" icon="mdi-close" @click="isOpen = false" />
-      </VToolbar>
-      <VRadioGroup
-        :model-value="activeTheme"
-        class="px-4"
-        hide-details
-        @update:model-value="setTheme"
-      >
-        <div
-          v-for="item in themeItems"
-          :key="item.value"
-          class="d-flex align-center"
+    <template #default="{ isActive }">
+      <VCard>
+        <VCardTitle class="d-flex justify-space-between align-center">
+          Theme selector
+          <VBtn
+            aria-label="Close"
+            density="comfortable"
+            icon="mdi-close"
+            variant="text"
+            @click="isActive.value = false"
+          />
+        </VCardTitle>
+        <VRadioGroup
+          :model-value="activeTheme"
+          class="px-4"
+          hide-details
+          @update:model-value="setTheme"
         >
-          <VRadio :label="item.title" :value="item.value" />
-          <VSpacer />
-          <template v-if="item.removable">
-            <VBtn
-              aria-label="Edit theme"
-              color="primary"
-              size="x-small"
-              variant="text"
-              icon
-              @click.stop="themeForm?.openEdit(item.title)"
-            >
-              <VIcon icon="mdi-square-edit-outline" size="large" />
-            </VBtn>
-            <VBtn
-              aria-label="Remove theme"
-              color="error"
-              size="x-small"
-              variant="text"
-              icon
-              @click.stop="removeCustomTheme(item.title)"
-            >
-              <VIcon icon="mdi-trash-can-outline" size="large" />
-            </VBtn>
-          </template>
-        </div>
-      </VRadioGroup>
-      <VCardText>
-        <VSheet
-          v-for="color in activeColors"
-          :key="color"
-          :color="color"
-          class="px-4 py-2 mb-1 text-label-large"
-          border
-          rounded
-        >
-          {{ color }}
-        </VSheet>
-      </VCardText>
-      <VDivider />
-      <VCardActions>
-        <VBtn
-          prepend-icon="mdi-plus"
-          text="Add custom theme"
-          variant="text"
-          @click="themeForm?.openAdd()"
-        />
-      </VCardActions>
-    </VCard>
-    <ThemeForm ref="themeForm" />
+          <div
+            v-for="item in themeItems"
+            :key="item.value"
+            class="d-flex align-center"
+          >
+            <VRadio :label="item.title" :value="item.value" />
+            <VSpacer />
+            <template v-if="item.removable">
+              <VBtn
+                aria-label="Edit theme"
+                color="primary"
+                size="x-small"
+                variant="text"
+                icon
+                @click.stop="themeForm?.openEdit(item.title)"
+              >
+                <VIcon icon="mdi-square-edit-outline" size="large" />
+              </VBtn>
+              <VBtn
+                aria-label="Remove theme"
+                color="error"
+                size="x-small"
+                variant="text"
+                icon
+                @click.stop="removeCustomTheme(item.title)"
+              >
+                <VIcon icon="mdi-trash-can-outline" size="large" />
+              </VBtn>
+            </template>
+          </div>
+        </VRadioGroup>
+        <VCardText>
+          <VSheet
+            v-for="color in activeColors"
+            :key="color"
+            :color="color"
+            class="px-4 py-2 mb-1 text-label-large"
+            border
+            rounded
+          >
+            {{ color }}
+          </VSheet>
+        </VCardText>
+        <VDivider />
+        <VCardActions>
+          <VBtn
+            prepend-icon="mdi-plus"
+            text="Add custom theme"
+            variant="text"
+            @click="themeForm?.openAdd()"
+          />
+        </VCardActions>
+      </VCard>
+      <ThemeForm ref="themeForm" />
+    </template>
   </VDialog>
 </template>
 
